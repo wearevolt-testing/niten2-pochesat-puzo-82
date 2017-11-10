a63d93e99447c38f5b4937be0a216ab4b97e025d
Niten <niten2@yandex.ru>
2017-11-10 17:21:37 +0700

    start

---
+++ web/public/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_type = 'mysqli';
+$db_host = 'mysql';
+$db_name = 'test';
+$db_username = 'dev';
+$db_password = 'dev';
+$db_prefix = '';
+$p_connect = false;
+
+$cookie_name = 'pun_cookie_42dd19';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = '199a3970923203a0';
+
+define('PUN', 1);
